83d738ac272b9a652f83ea394bc9882cea12da3e
Luis.Giusti <luis.giusti@kuehne-nagel.com>
2018-09-19 16:33:01 -0300

    Updated GET/POST actions. Prepared project for app preferences.

--- app/src/main/java/exam/luisgiusti/magnetocodebreaker/networkTasks/GetNetworkAsyncTask.kt
+++ app/src/main/java/exam/luisgiusti/magnetocodebreaker/networkTasks/GetNetworkAsyncTask.kt
@@ -6,12 +6,17 @@ import exam.luisgiusti.magnetocodebreaker.model.Stats
 class GetNetworkAsyncTask : AsyncTask <Void, Void, Stats>() {
 
     override fun doInBackground(vararg p0: Void?): Stats {
-        val response = khttp.get("http://meli.giusti.net.ar/stats/")
-        val result = response.jsonObject
-        return when {
-            response.statusCode == 200 -> Stats(result["countMutantDna"].toString().toLong(), result["countHumanDna"].toString().toLong())
-            else -> Stats()
+        try {
+            val response = khttp.get("http://meli.giusti.net.ar/stats/")
+            val result = response.jsonObject
+            return when {
+                response.statusCode == 200 -> Stats(result["count_mutant_dna"].toString().toLong(), result["count_human_dna"].toString().toLong())
+                else -> Stats()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
+        return Stats()
     }
 
 }
